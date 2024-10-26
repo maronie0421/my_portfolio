88de3ae57b25ac4244bdecd3b0ada5a95717ed49
maronie0421 <maro.v.cookie@gmail.com>
2024-10-27 00:30:06 +0900

    トップへ戻るボタン完了

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ inViewItems.forEach(function(inViewItem) {
   inViewItems
 });
 
-//ハンバーガーボタン動作
+//ハンバーガーボタン動作 Javascript
 document
   .querySelector("#js-drawer-button")
   .addEventListener("click", function(e) {
@@ -24,3 +24,24 @@ document
     document.querySelector("#js-drawer-button").classList.toggle("is-checked");
     document.querySelector("#js-drawer-content").classList.toggle("is-checked");
   });
+
+
+
+  //TOPへ戻るボタン jQuery
+  jQuery(function() {
+    jQuery("#js-button-top").hide(); // ボタンを初期状態で非表示
+
+    jQuery(window).scroll(function() {
+      if (jQuery(this).scrollTop() > 100) {
+        jQuery("#js-button-top").fadeIn(); // スクロール位置が100pxを超えたらフェードイン
+      } else {
+        jQuery("#js-button-top").fadeOut(); // 100px未満になったらフェードアウト
+      }
+    });
+
+    jQuery("#js-button-top").on("click", function () {
+      jQuery("html,body").animate({ scrollTop: 0 },100);
+      return false;
+    });
+  });
+
